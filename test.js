c4b9d987e1453d9a02c769daace399f24533cb16
木澤朋和 <kizawa@kizawatomoowanoMacBook-Pro.local>
2017-01-04 09:26:10 +0900

    最初の登録

---
+++ test.js
@@ -0,0 +1,43 @@
+// alert("Hello World!!");
+console.log("コンソール出力TEST");
+var a=100;
+var b=200;
+var c=0;
+c=a+b;
+console.log(c);
+var num=[1, 2, 3];
+console.log(num[0]);
+
+if(a<b)
+{
+    console.log("bの方が大きい",a,b);
+}
+else
+{
+    conso.log("aの方が大きい");
+}
+
+
+func(1,2);
+console.log(func2(2,3));
+func3();
+// console.log(x1);
+func3();
+
+function func(x,y)
+{
+    console.log(x+y);
+}
+
+function func2(x,y)
+{
+    return(x+y);
+}
+
+function func3()
+{
+    var x1=1;
+    var y1=2;
+
+    console.log(x1+y1);
+}
